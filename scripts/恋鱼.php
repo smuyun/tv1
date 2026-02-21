7305004ea7b74022ce183bf5f1b53f6e5a49ce52
smuyun <117051233+smuyun@users.noreply.github.com>
2026-02-21 21:22:42 +0800

    Add files via upload

---
+++ scripts/恋鱼.php
@@ -0,0 +1,171 @@
+{
+	 "list" : [{
+    "vod_name": "一英里",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260220-1/b0cffcbbd10858f20a9a9e3dc5c2bb30.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2MzU3LCPkuIDoi7Hph4w="
+},{
+    "vod_name": "可怖之事",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260220-1/c924bf8e40b641c5ab14da2b6cb61656.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2MzUzLCPlj6/mgJbkuYvkuos="
+},{
+    "vod_name": "再一英里",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260220-1/ba4a88aa850b2ddedf96f4b122250c0a.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2MzU0LCPlho3kuIDoi7Hph4w="
+},{
+    "vod_name": "孔雀舞曲",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260220-1/c581fd8ae50a47e6a30b26a6dd286b49.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2MzQyLCPlrZTpm4DoiJ7mm7I="
+},{
+    "vod_name": "寻秦记粤语",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260221-1/ba405c557b40e6fe77eb6a04cc300be2.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2Mzk1LCPlr7vnp6borrDnsqTor60="
+},{
+    "vod_name": "寻秦记国语",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260221-1/ba405c557b40e6fe77eb6a04cc300be2.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2Mzk2LCPlr7vnp6borrDlm73or60="
+},{
+    "vod_name": "浴火之森",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260220-1/96a8a07c8bbc2562d059c3a349990858.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2MzUwLCPmtbTngavkuYvmo64="
+},{
+    "vod_name": "过家家",
+    "vod_pic": "https://viptulz.com/upload/vod/20260220-1/c32aba65604ee86a02fee7391c179ed7.webp",
+    "vod_remarks": "HD",
+    "vod_id": "MTcwMTUwLCPov4flrrblrrY="
+},{
+    "vod_name": "桑莫儿：璀璨本色",
+    "vod_pic": "https://viptulz.com/upload/vod/20260220-1/37737650f5de3c7fc96518dc342341b9.webp",
+    "vod_remarks": "HD",
+    "vod_id": "MjE2MDc2LCPmoZHojqvlhL/vvJrnkoDnkqjmnKzoibI="
+},{
+    "vod_name": "消失的证据",
+    "vod_pic": "https://viptulz.com/upload/vod/20260221-1/1b9445e5019e0a05be6a14af82e8a679.webp",
+    "vod_remarks": "HD",
+    "vod_id": "MjE2MDE1LCPmtojlpLHnmoTor4Hmja4="
+},{
+    "vod_name": "熊猫计划之部落奇遇记",
+    "vod_pic": "https://viptulz.com/upload/vod/20260220-1/92b4f7ff953ebeb4046062bc1ef2fb20.webp",
+    "vod_remarks": "TC",
+    "vod_id": "MjE2MjcyLCPnhornjKvorqHliJLkuYvpg6jokL3lpYfpgYforrA="
+},{
+    "vod_name": "瑞典救援暗线",
+    "vod_pic": "https://viptulz.com/upload/vod/20260220-1/954c5f3608afdf98eef31f07a025dee7.webp",
+    "vod_remarks": "HD",
+    "vod_id": "MjE2MjM5LCPnkZ7lhbjmlZHmj7Tmmpfnur8="
+},{
+    "vod_name": "不被爱也没关系",
+    "vod_pic": "https://viptulz.com/upload/vod/20260221-1/9c8e05ceefcbea9d690a24b284244e36.webp",
+    "vod_remarks": "HD",
+    "vod_id": "MjE2MjEzLCPkuI3ooqvniLHkuZ/msqHlhbPns7s="
+},{
+    "vod_name": "东北警察故事3",
+    "vod_pic": "https://viptulz.com/upload/vod/20260221-1/b5ed8a8299b396e9b226e75e4daa1953.webp",
+    "vod_remarks": "HD",
+    "vod_id": "MjE2MTI5LCPkuJzljJforablr5/mlYXkuosz"
+},{
+    "vod_name": "春行",
+    "vod_pic": "https://viptulz.com/upload/vod/20260220-1/e8b56a463a5a7b12fba1a67963d7b4dc.webp",
+    "vod_remarks": "HD",
+    "vod_id": "NDc0MTEsIabcdaYpeihjA=="
+},{
+    "vod_name": "我们埋葬死者",
+    "vod_pic": "https://viptulz.com/upload/vod/20260221-1/6302fe8bb4e81864d72e1bb5c67fe06c.webp",
+    "vod_remarks": "HD",
+    "vod_id": "MjMyMTEsIabcdaIkeS7rOWfiabcdiRrOatuabcdiAhQ=="
+},{
+    "vod_name": "寻秦记",
+    "vod_pic": "https://viptulz.com/upload/vod/20260221-1/15f7d7c303bf000d36e5b211b803df7f.webp",
+    "vod_remarks": "HD",
+    "vod_id": "MTU1NDQ3LCPlr7vnp6borrA="
+},{
+    "vod_name": "马上有喜人",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260218-1/96190db13db1e131b26cb6e33ee79c87.jpg",
+    "vod_remarks": "第5期",
+    "vod_id": "MjE2MDY1LCPpqazkuIrmnInllpzkuro="
+},{
+    "vod_name": "通天之塔",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260213-1/870916eb9874e0aee859482334ef09ac.jpg",
+    "vod_remarks": "第24集完结",
+    "vod_id": "MjE1NTU0LCPpgJrlpKnkuYvloZQ="
+},{
+    "vod_name": "星缘奇遇",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260221-1/d487e4fe99a0d4b94c3efc928a01bbff.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2NDU5LCPmmJ/nvJjlpYfpgYc="
+},{
+    "vod_name": "独子",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260221-1/8fc841365b34b1023ad8b2ab0daaf80f.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2Mzk5LCPni6zlrZA="
+},{
+    "vod_name": "关你茶室",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260221-1/16cc0580656e41bb0ae893a84d932541.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2MzcxLCPlhbPkvaDojLblrqQ="
+},{
+    "vod_name": "游轮谋杀案",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260221-1/733f2c14756e7431fa06d02b987f0530.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2NDc0LCPmuLjova7osIvmnYDmoYg="
+},{
+    "vod_name": "千密千危",
+    "vod_pic": "https://dbzy5.com/upload/vod/20260221-1/9f31282004d59f827053e7c09deca37b.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2NDU4LCPljYPlr4bljYPljbE="
+},{
+    "vod_name": "一路福星",
+    "vod_pic": "https://wework.qpic.cn/wwpic3az/153041_jQ8lXXJsR4eub0F_1771414428/0",
+    "vod_remarks": "HD中字",
+    "vod_id": "MjE2MDg5LCPkuIDot6/npo/mmJ8="
+},{
+    "vod_name": "得闲谨制",
+    "vod_pic": "https://vcover-vt-pic.puui.qpic.cn/vcover_vt_pic/0/mzc00200yi8ktmx1764292823932/350",
+    "vod_remarks": "6.9",
+    "vod_id": "NDk2MjgsIabcdWabcdlabcdmXsuiwqOWItg=="
+},{
+    "vod_name": "Y计划",
+    "vod_pic": "http://img.bwcgee.cn/friday/drama/20260209/o_eb07b1977b4c4216ad2a94895bce0975.jpg",
+    "vod_remarks": "超清",
+    "vod_id": "MjE0Mzc0LCNZ6K6h5YiS"
+},{
+    "vod_name": "火拼",
+    "vod_pic": "http://pic4.iqiyipic.com/image/20260211/ad/d3/v_193179832_m_601_m4_1248_702.jpg",
+    "vod_remarks": "1080P",
+    "vod_id": "MjE0ODk2LCPngavmi7w="
+},{
+    "vod_name": "爷孙大学路",
+    "vod_pic": "http://img.bwcgee.cn/friday/drama/20260128/o_d0a6e8658efe45e8887e16da01fa4b86.webp",
+    "vod_remarks": "超清",
+    "vod_id": "MjE1NTMwLCPniLflrZnlpKflrabot68="
+},{
+    "vod_name": "李逵断案",
+    "vod_pic": "https://images.67c6c7a.com/upload/vod/2026-02/202602201771583742.jpg",
+    "vod_remarks": "正片",
+    "vod_id": "MjE2MzU1LCPmnY7pgLXmlq3moYg="
+}],
+	 	"filters":{},
+	"code": 1,
+	"msg": 200,
+	"class": [{
+    "type_name": "电影",
+    "type_id": "1"
+},{
+    "type_name": "电视剧",
+    "type_id": "2"
+},{
+    "type_name": "综艺",
+    "type_id": "3"
+},{
+    "type_name": "动漫",
+    "type_id": "4"
+},{
+    "type_name": "短剧",
+    "type_id": "20"
+}]}
